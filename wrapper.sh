cebf333dcfffa1aa671cc7b4f2f1aee9593a7148
LVhengani <lvhengani@csir.co.za>
2016-05-16 15:58:45 +0200

    Lowered the resolution from 10m to 20m

--- wrapper.sh
+++ wrapper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source /root/sen2cor/L2A_Bashrc
-exec L2A_Process --resolution 10 --profile /var/sentinel2_data/"$1"
+#exec L2A_Process --resolution 10 --profile /var/sentinel2_data/"$1"
+exec L2A_Process --resolution 20 /var/sentinel2_data/"$1"
